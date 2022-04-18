13c10d5b52730c4182f58d6e8802e44c44f614a9
kenedfelix <kened.felix@gmail.com>
2022-04-18 15:26:51 -0300

    Listado as transações da api

--- dtmoney/src/components/TransactionsTable/index.tsx
+++ dtmoney/src/components/TransactionsTable/index.tsx
@@ -1,12 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import {Container} from "./styles";
 
+interface Transaction {
+    id:number;
+    title:string;
+    amount: number;
+    type: string;
+    category: string;
+    createdAt: string;
+}
 export function TransactionTable(){
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+
     useEffect(() => {
         api.get('/transactions')
 
-        .then(response => console.log(response.data))
+        .then(response => setTransactions(response.data.transactions))
     }, []);
     return (
         <Container>
@@ -20,30 +30,14 @@ export function TransactionTable(){
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Desenvolvimento de website</td>
-                        <td className="deposit">R$ 12,000</td>
-                        <td>Desenvolvoimetno</td>
-                        <td>20/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Aluguel</td>
-                        <td className="withDraw">- R$ 900,00</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Desenvolvimento de website</td>
-                        <td>R$ 12,000</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Desenvolvimento de website</td>
-                        <td>R$ 12,000</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
+                   {transactions.map(transaction => (
+                       <tr key={transaction.id}>
+                        <td>{transaction.title}</td>
+                        <td className={transaction.type}>{transaction.amount}</td>
+                        <td>{transaction.category}</td>
+                        <td>{transaction.createdAt}</td>
+                       </tr>
+                   ))}
                 </tbody>
             </table>
         </Container>
--- dtmoney/src/index.tsx
+++ dtmoney/src/index.tsx
@@ -8,6 +8,27 @@ createServer({
   models: {
     transaction: Model,
   },
+
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withDraw',
+          amount: 1000,
+          createdAt: new Date('2021-02-12 014:00:00'),
+        },
+      ]
+    })
+  },
   routes(){
     this.namespace = 'api';
 
