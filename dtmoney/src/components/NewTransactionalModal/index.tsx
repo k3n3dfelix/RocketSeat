616100a2aa330fc64e385d320e6a71207ea06223
kenedfelix <kened.felix@gmail.com>
2022-04-18 15:07:43 -0300

    Cadastrando os dados das transações na FakeApi

--- dtmoney/src/components/NewTransactionalModal/index.tsx
+++ dtmoney/src/components/NewTransactionalModal/index.tsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
+import { api } from '../../services/api';
 import { Container, RadioBox, TransactionalTypeContainer } from './styles';
 
 interface NewTransactionalModalProps{
@@ -11,10 +12,23 @@ interface NewTransactionalModalProps{
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionalModalProps){
-
+    
+    const [title, setTitle] = useState('');
+    const [value, setValue] = useState(0);
+    const [category, setCategory] = useState('');
     const [type, setType] = useState('deposit');
 
-    
+    function handleCreateNewTransaction(event: FormEvent) {
+        event.preventDefault();
+        const data = {
+            title,
+            value,
+            category,
+            type
+        };
+
+        api.post('/transactions', data)
+    }
     
     return (
     <Modal 
@@ -27,20 +41,20 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionalMo
             <img src={closeImg} alt="Fechar Modal"/>
         </button>
 
-        <Container>
+        <Container onSubmit={handleCreateNewTransaction}>
             <h2>Cadastrar Transação</h2>
 
-            <input placeholder="Título"/>
-            <input type="number" placeholder="Valor" />
+            <input placeholder="Título" value={title} onChange={event => setTitle(event.target.value)}/>
+            <input type="number" placeholder="Valor" value={value} onChange={event => setValue(Number(event.target.value))}/>
             <TransactionalTypeContainer>
-                <RadioBox type="button"  onClick={() => {setType('deposit');}} isActive={type == 'deposit'}>
+                <RadioBox type="button"  onClick={() => {setType('deposit');}} isActive={type == 'deposit'} activeColor="green">
                     <img src={incomeImg}  alt="Entrada"/><span>Entrada</span>
                 </RadioBox>
-                <RadioBox type="button"  onClick={() => {setType('withdraw')}} isActive={type == 'withdraw'}>
+                <RadioBox type="button"  onClick={() => {setType('withdraw')}} isActive={type == 'withdraw'} activeColor="red">
                     <img src={outcomeImg} /><span>Saída</span>
                 </RadioBox>
             </TransactionalTypeContainer>
-            <input type="text" placeholder="Categoria" />
+            <input type="text" placeholder="Categoria" value={category} onChange={event => setCategory(event.target.value)} />
             <button type="submit">Cadastrar</button>
            
         
--- dtmoney/src/components/NewTransactionalModal/styles.ts
+++ dtmoney/src/components/NewTransactionalModal/styles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-import {darken} from 'polished';
+import {darken, transparentize} from 'polished';
+
 
 
 export const Container = styled.form `
@@ -59,12 +60,18 @@ export const TransactionalTypeContainer = styled.div `
 
 interface RadioBoxProps{
     isActive: boolean;
+    activeColor: 'green' | 'red';
+}
+
+const colors = {
+    green: '#33CC95',
+    red: '#E52E4D'
 }
 export const RadioBox = styled.button <RadioBoxProps>`
     height: 4rem;
     border: 1px solid #d7d7d7;
     border-radius: 0.25rem;
-    background: ${(props) => props.isActive ? '#ccc' : 'transparent'};
+    background: ${(props) => props.isActive ? transparentize(0.9, colors[props.activeColor]) : 'transparent'};
 
     display: flex;
     align-items: center;
