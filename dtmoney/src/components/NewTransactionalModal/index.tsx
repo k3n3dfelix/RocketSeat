416e3b79d468a02f04cd53c7b19a10ef04519b46
kenedfelix <kened.felix@gmail.com>
2022-04-18 12:58:25 -0300

    Feito Estilização do modal e adicionado o helper polished

--- dtmoney/src/components/NewTransactionalModal/index.tsx
+++ dtmoney/src/components/NewTransactionalModal/index.tsx
@@ -1,4 +1,8 @@
 import Modal from 'react-modal';
+import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import { Container, TransactionalTypeContainer } from './styles';
 
 interface NewTransactionalModalProps{
     isOpen: boolean;
@@ -7,11 +11,34 @@ interface NewTransactionalModalProps{
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionalModalProps){
     return (
-        <Modal 
+    <Modal 
         isOpen={isOpen} 
         onRequestClose={onRequestClose}
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content"
     >
-        <h2>Cadastrar Transação</h2>
+        <button type="button" onClick={onRequestClose} className="react-modal-close">
+            <img src={closeImg} alt="Fechar Modal"/>
+        </button>
+
+        <Container>
+            <h2>Cadastrar Transação</h2>
+
+            <input placeholder="Título"/>
+            <input type="number" placeholder="Valor" />
+            <TransactionalTypeContainer>
+                <button type="button">
+                    <img src={incomeImg}  alt="Entrada"/><span>Entrada</span>
+                </button>
+                <button type="button">
+                    <img src={outcomeImg} /><span>Saída</span>
+                </button>
+            </TransactionalTypeContainer>
+            <input type="text" placeholder="Categoria" />
+            <button type="submit">Cadastrar</button>
+           
+        
+        </Container>
     </Modal>
     );
 }
