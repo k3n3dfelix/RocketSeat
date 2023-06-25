77c468a3d75ba4895d9423b6e46f720185ebfa28
kenedfelix <kened.felix@gmail.com>
2023-06-24 22:22:31 -0300

    Treinamento redux

---
+++ react-redux-zustand/src/components/TodoList.tsx
@@ -0,0 +1,13 @@
+import { useAppSelector } from '../store';
+
+export function TodoList(){
+
+    const todos  = useAppSelector(store => {
+        return store.todo
+    })
+    return(
+        <ul>
+           {todos.map(todo => <li key={todo}>{todo}</li>)}
+        </ul>
+    )
+}
